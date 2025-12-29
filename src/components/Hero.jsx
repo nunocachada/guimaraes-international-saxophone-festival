e88de74bb25bc16e523af11ac90d393e43803dbd
Nuno Miguel Borges Sá Cachada <nunocachada@gmail.com>
2025-12-29 14:20:13 +0000

    Updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -51,7 +51,7 @@ export function Hero() {
           >
             Participar
           </Button> */}
-          <Formulario className="mt-10 sm:hidden" />
+          {/* <Formulario className="mt-10 sm:hidden" /> */}
           <dl className="mt-10 grid grid-cols-2 gap-x-10 gap-y-6 text-lg uppercase sm:mt-16 sm:gap-x-16 sm:gap-y-10 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left">
             {[
               // ['Artistas', '18'],
