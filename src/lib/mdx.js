577083e136a18810e211c5f07ff8e885065b4401
Nuno Miguel Borges Sá Cachada <nunocachada@gmail.com>
2026-06-23 00:09:30 +0200

    Updates

--- src/lib/mdx.js
+++ src/lib/mdx.js
@@ -1,28 +1,27 @@
-import glob from 'fast-glob'
+const NOTICIA_SLUGS = [
+  'boas-vindas',
+  'henk-van-twillert',
+  'nicolas-arsenijevic',
+  'ties-mellema',
+]
 
-async function loadEntries(directory, metaName) {
+async function loadEntries(directory, metaName, slugs) {
   return (
     await Promise.all(
-      (await glob('**/page.mdx', { cwd: `src/app/${directory}` })).map(
-        async (filename) => {
-          let metadata = (await import(`../app/${directory}/${filename}`))[
-            metaName
-          ]
-          return {
-            ...metadata,
-            metadata,
-            href: `/${directory}/${filename.replace(/\/page\.mdx$/, '')}`,
-          }
-        },
-      ),
+      slugs.map(async (slug) => {
+        const mod = await import(`../app/${directory}/${slug}/page.mdx`)
+        const metadata = mod[metaName]
+
+        return {
+          ...metadata,
+          metadata,
+          href: `/${directory}/${slug}`,
+        }
+      }),
     )
   ).sort((a, b) => b.data.localeCompare(a.data))
 }
 
 export function carregarNoticias() {
-  return loadEntries('noticias', 'noticia')
+  return loadEntries('noticias', 'noticia', NOTICIA_SLUGS)
 }
-
-// export function loadCaseStudies() {
-//   return loadEntries('work', 'caseStudy')
-// }
